1b95bd4a570a6e962f436f7d566505a052f4a9f3
simon aubertin <simonaubertin1994@gmail.com>
2023-10-10 21:28:57 -0400

    we did the same thing as last commit with the price variable, quantity variable

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 class Item:
-    def __init__(self, name):
+    def __init__(self, name, price, quantity):
         self.name = name
-        print(f"an instance created : {name}")
+        self.price = price
+        self.quantity = quantity
 
     def calculate_total_price(self, price, quantity):
         return self.price * self.quantity
 
-item1 = Item("Phone")
-item1.price = 100
-item1.quantity = 5
+item1 = Item("Phone", 100, 5)
 
-
-item2 = Item("Computer")
-item2.price = 2500
-item2.quantity = 2
+item2 = Item("Computer", 2500, 2)
 
 print(item1.name)
-print(item2.name)
+print(item1.price)
+print(item1.quantity)
