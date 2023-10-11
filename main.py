d36cddb558a5bdbe35af2e10f9b07b7c0c5bc3ba
simon aubertin <simonaubertin1994@gmail.com>
2023-10-10 22:06:57 -0400

    we use assert on our price and quantity instance attribute so we can tell the user more specificaly what they done wrong. We also added a class attribute pay_rate to calculate discount

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 class Item:
-    def __init__(self, name, price, quantity = 0):
+    pay_rate = 0.8# the pay rate after 20% discount
+
+    def __init__(self, name: str, price: float, quantity = 0):
+        #Run validation to the received arguments
+        assert price >= 0, f" Price {price} is not greater than zero"
+        assert quantity >= 0, f"Quantity {quantity} is not greater than zero"
+
+        #assign to self object
         self.name = name
         self.price = price
         self.quantity = quantity
 
     def calculate_total_price(self):
         return self.price * self.quantity
+    
+    def apply_discount(self):
+        pass
 
 item1 = Item("Phone", 100, 5)
-
 item2 = Item("Computer", 2500, 2)
-item2.has_numpad = False
 
-print(item1.name)
-print(item1.price)
-print(item1.quantity)
-print(item1.calculate_total_price())
+print(Item.__dict__) #all attribute of the class level  
+print(item1.__dict__) # all attribute of the instance level
