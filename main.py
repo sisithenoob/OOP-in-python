5d5f02842adf049804529ed54bdfdeb169abc249
simon aubertin <simonaubertin1994@gmail.com>
2023-10-10 20:46:40 -0400

    here we see that we cannot just use variable to keep track of our items, it woulb be better if all of this would belong to only one data type

---
+++ main.py
@@ -0,0 +1,9 @@
+item_1 = "phone"
+item_1_price = 100
+item_1_quantity = 5
+item_1_price_total = item_1_price * item_1_quantity
+
+print(type(item_1))
+print(type(item_1_price))
+print(type(item_1_quantity))
+print(type(item_1_price_total))
