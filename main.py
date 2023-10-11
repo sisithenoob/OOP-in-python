b52342ec3e39d44add1f121305c378aa3f912899
simon aubertin <simonaubertin1994@gmail.com>
2023-10-10 21:24:54 -0400

    we instanciated the name variable with the __init__()

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 class Item:
-    def __init__(self):
-        print("I am created !!!!")
+    def __init__(self, name):
+        self.name = name
+        print(f"an instance created : {name}")
 
     def calculate_total_price(self, price, quantity):
         return self.price * self.quantity
 
-item1 = Item()
-item1.name = "phone"
+item1 = Item("Phone")
 item1.price = 100
 item1.quantity = 5
-total_price_item1 =  item1.calculate_total_price(item1.price, item1.quantity)
-print(total_price_item1)
 
-item2 = Item()
-item2.name = "computer"
+
+item2 = Item("Computer")
 item2.price = 2500
 item2.quantity = 2
-total_price_item2 = item2.calculate_total_price(item2.price, item2.quantity)
-print(total_price_item2)
+
+print(item1.name)
+print(item2.name)
