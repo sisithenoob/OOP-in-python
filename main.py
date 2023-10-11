825ae84f75501de4b6a5c27366b8d4c7dae02f67
simon aubertin <simonaubertin1994@gmail.com>
2023-10-10 21:02:43 -0400

    we create 2 instances of our item class and we wrote our first method calculate_total_price()

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 class Item:
-    pass
+    def calculate_total_price(self, price, quantity):
+        return self.price * self.quantity
 
 item1 = Item()
-print(type(item1))
+item1.name = "phone"
+item1.price = 100
+item1.quantity = 5
+total_price_item1 =  item1.calculate_total_price(item1.price, item1.quantity)
+print(total_price_item1)
+
+item2 = Item()
+item2.name = "computer"
+item2.price = 2500
+item2.quantity = 2
+total_price_item2 = item2.calculate_total_price(item2.price, item2.quantity)
+print(total_price_item2)
