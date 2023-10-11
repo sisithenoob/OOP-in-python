96ab74535045cc2cf0b9c45dea8f7b6f861f6c77
simon aubertin <simonaubertin1994@gmail.com>
2023-10-10 20:52:58 -0400

    as we seen the format of using only variable didnt work so, here we have implemented a class item so all the important information about our item can be deal with the same data type

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-item_1 = "phone"
-item_1_price = 100
-item_1_quantity = 5
-item_1_price_total = item_1_price * item_1_quantity
+class Item:
+    pass
 
-print(type(item_1))
-print(type(item_1_price))
-print(type(item_1_quantity))
-print(type(item_1_price_total))
+item1 = Item()
+print(type(item1))
